2c35044f582ab7f751fd94f023f22790678ed981
farrukh-malik <farrukhmalik46@gmail.com>
2019-09-30 04:45:15 +0500

    ready

--- src/component/user-entry-section.js
+++ src/component/user-entry-section.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import Instruction from './instruction';
-
 import './user-entry-section.css';
 import Kazuya from '../images/Kazuya.png';
 import Jinnn from '../images/jin1.png';
-const UserEntrySection = () => {
-    return (
-        <div>
-            <div className="Form">
-                <input type="text" placeholder="Enter Your Name" />
-                <button><Link to="/instruction">PLAY NOW</Link></button>
+class UserEntrySection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div className="Form">
+                    <input type="text" value={this.state.inputValue} onChange={this.updateInputValue} placeholder="Enter Your Name" />
+                    <button><Link to="/instruction">PLAY NOW</Link></button>
+                </div>
+                <img src={Kazuya} className="Kazuya" alt="" />
+                <img src={Jinnn} className="Jinnn" alt="" />
             </div>
-            <img src={Kazuya} className="Kazuya" alt="" />
-            <img src={Jinnn} className="Jinnn" alt="" />
-        </div>
-    );
-};
+        );
+    }
+
+    updateInputValue = (evt) => {
+        this.setState({
+            inputValue: evt.target.value
+        });
+        console.log("farrukh", this.state.inputValue);
+        localStorage.setItem('name', this.state.inputValue)
+    }
+}
+
+// export default ;
+// const UserEntrySection = () => {
+//     return (
+//         <div>
+//             <div className="Form">
+//                 <input type="text" value={this.state.inputValue} onChange={this.updateInputValue} placeholder="Enter Your Name" />
+//                 <button><Link to="/instruction">PLAY NOW</Link></button>
+//             </div>
+//             <img src={Kazuya} className="Kazuya" alt="" />
+//             <img src={Jinnn} className="Jinnn" alt="" />
+//         </div>
+//     );
+// };
 
 export default UserEntrySection;
--- src/component/win-screen.js
+++ src/component/win-screen.js
@@ -1,25 +1,64 @@
-import React from "react";
+// import React from "react";
 import jingif from './../images/jingiff.gif';
 import paulgif from './../images/paulgif.gif';
 import './win-screen.css';
-const win = () => {
-  return (
-    <div>
-      <div className="WinContainer">
-        <h1>Congratulations</h1>
-        <h2>Player</h2>
-        <p>you are quiz master!!</p>
-        <div className="Gif">
-            <img src={jingif}/>
-            <img src={paulgif}/>
-        </div>
-        <div className="WinBtn">
-          <button>Main Menu</button>
-          <button>Credits</button>
+import React, { Component } from 'react';
+class win extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ""
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="WinContainer">
+          <h1>Congratulations</h1>
+          <h2>{this.state.inputValue}</h2>
+          <p>you are quiz master!!</p>
+          <div className="Gif">
+            <img src={jingif} />
+            <img src={paulgif} />
+          </div>
+          <div className="WinBtn">
+            <button>Main Menu</button>
+            <button>Credits</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  async componentWillMount() {
+
+    const name = localStorage.getItem('name')
+    await this.setState({
+      inputValue: name
+    });
+  }
+}
 
 export default win;
+
+// const win = () => {
+//   return (
+//     <div>
+//       <div className="WinContainer">
+//         <h1>Congratulations</h1>
+//         <h2>Player</h2>
+//         <p>you are quiz master!!</p>
+//         <div className="Gif">
+//             <img src={jingif}/>
+//             <img src={paulgif}/>
+//         </div>
+//         <div className="WinBtn">
+//           <button>Main Menu</button>
+//           <button>Credits</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default win;
