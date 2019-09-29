ea68a29c9aae3547899b4c448cab47df3daeb8dc
shariqahmed123 <barkat.mgf@gmail.com>
2019-09-29 03:25:20 -0700

    instruction and win screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TopicSelection from './Component/topic-selection';
-import UserEntrySection from './Component/user-entry-section';
+import TopicSelection from './component/topic-selection';
+import UserEntrySection from './component/user-entry-section';
+import Game from './component/game-section';
+import Instruction from './component/instruction'; 
+import WinScreen from './component/win-screen';
+
+
 
 function App() {
   return (
     <div className="App">
-     
+     {/* <Game/>  */}
+     {/* <Instruction /> */}
+     {/* <TopicSelection/> */}
+     {/* <UserEntrySection/> */}
+     <WinScreen/>
     </div>
   );
 }
---
+++ src/component/game-section.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./game-section.css";
+
+const Game = () => {
+  return (
+    <div className="QuizArea">
+      <div className="QuizHeader"></div>
+      <h1>Questions</h1>
+      <div className="QuizContent">
+        <p>Lorem Lorem Lorem LoremLorem Lorem Lorem Lorem Lorem Lorem?</p>
+        <div className="QuizBtn1">
+          <button>haha</button>
+          <button>haha</button>
+        </div>
+        <div className="QuizBtn2">
+          <button>haha</button>
+          <button>haha</button>
+        </div>
+        <div className="QuizProceed">
+          <button>GO</button>
+        </div>
+      </div>
+      <div className="QuizFooter"></div>
+    </div>
+  );
+};
+
+export default Game;
